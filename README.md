a24b2f4d115a714ecd9ebacc57006db84a89781b
Levitre <levitre.mathys30@gmail.com>
2026-02-05 14:17:42 +0100

    Project base

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# r411alto
+# Alto
 
 A new Flutter project.
 
---
+++ android/app/src/main/kotlin/com/example/r411alto/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.r411alto
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
