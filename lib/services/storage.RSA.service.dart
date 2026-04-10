f496a43194c9f7773e966d09a493d690bd75dace
MaxenceLAURENCE <maxence.laurence.pro@gmail.com>
2026-04-10 15:41:48 +0200

    Backend qrcode (#20)
    * feat(rsa): ajout de fonction pour chiffrer les message avec RSA
    
    * feat(qrcode) : génération du qrcode + ajout d'une variable globale urlBackend

---
+++ lib/services/storage.RSA.service.dart
@@ -0,0 +1,62 @@
+import 'dart:ffi';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class StorageRsaService {
+  /// StorageRsaService : gestion du stockage des clés RSA de l'application.
+
+  /// Création de l'instance du stockage.
+  final FlutterSecureStorage _storage;
+
+  StorageRsaService(this._storage);
+
+  /// Méthode de sauvegarde des clés RSA.
+  ///
+  /// Paramètres :
+  /// - relationId (String) : L'identifiant de la relation.
+  /// - publicKeyPem (String) : La clé publique au format PEM.
+  /// - privateKeyPem (String) : La clé privée au format PEM.
+  String _pubKey(String relationId) => 'rel:$relationId:pubPem';
+  String _privKey(String relationId) => 'rel:$relationId:privPem';
+
+  Future<void> saveKeyPair(
+    String relationId, {
+    required String publicKeyPem,
+    required String privateKeyPem,
+  }) async {
+    await _storage.write(key: _pubKey(relationId), value: publicKeyPem);
+    await _storage.write(key: _privKey(relationId), value: privateKeyPem);
+  }
+
+  String _distantPubKey(String relationId) => 'rel:$relationId:distantPubPem';
+
+  Future<void> saveDistantPublicKey(String relationId, String publicKeyPem) async {
+    await _storage.write(key: _distantPubKey(relationId), value: publicKeyPem);
+  }
+
+  Future<String?> readPublicKeyPem(String relationId) =>
+      _storage.read(key: _pubKey(relationId));
+
+  Future<String?> readPrivateKeyPem(String relationId) =>
+      _storage.read(key: _privKey(relationId));
+
+  /// Récupérer l'ID de la dernière relation enregistrée où null si pas de relation.
+  Future<int?> getLastRelationId() async {
+    final allKeys = await _storage.readAll();
+    int maxId = 0;
+
+    for (var key in allKeys.keys) {
+      if (key.startsWith('rel:')) {
+        int id = int.parse(key.split(':')[1]);
+        if (id > maxId) {
+          maxId = id;
+        }
+      }
+    }
+
+    if (maxId == 0) {
+      return null;
+    }
+    return maxId;
+  }
+}
